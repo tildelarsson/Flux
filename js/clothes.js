f60585c13a89d53ca31b3369a373609fbd53e4bb
tildelarsson <160611292+tildelarsson@users.noreply.github.com>
2024-03-14 09:58:29 +0100

    Initial commit

---
+++ js/clothes.js
@@ -0,0 +1,23 @@
+function buildMenu()
+{
+    var links = [
+    
+        ["Hem", "index.html"], 
+        ["Kläder", "clothes.html"], 
+        ["Våra designers", "designers.html"],
+        ["Kontakta oss", "kontakt.html"]
+    ]
+
+    var content =""
+
+    for(var i = 0; i <links.length; i++)
+    {
+        var text = links[i][0]
+        var adress = links[i][1]
+        content = content + "<li><a href=\"" + adress + "\">" + text + "</a></li>"
+
+        // <li><a href="index.html">Startsidan</a></li>
+    }
+
+    document.getElementById("menu").innerHTML = content
+    }
